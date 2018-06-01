772f1c54e80d7f317f7f23fd42d7cea59f71ca74
Adam Ploshay <aploshay@iu.edu>
2018-06-01 14:14:36 -0400

    Timestamp changes of db migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180531203362) do
+ActiveRecord::Schema.define(version: 20180601145018) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer "user_id", null: false
