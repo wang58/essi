3d5ad446dfccbfcf86baab07d00193c43c488fb7
Randall Floyd <rdfloyd@indiana.edu>
2018-06-28 16:04:14 -0400

    Adding a module with extended metadata properties that can be mixed into most Work Types

--- app/models/image.rb
+++ app/models/image.rb
@@ -8,6 +8,9 @@ class Image < ActiveFedora::Base
   # self.valid_child_concerns = []
   validates :title, presence: { message: 'Your work must have a title.' }
 
+  # Include extended metadata common to most work types
+  include Catorax::ExtendedMetadata
+
   # This must be included at the end, because it finalizes the metadata
   # schema (by adding accepts_nested_attributes)
   include ::Hyrax::BasicMetadata
