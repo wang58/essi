871217ef9f16f9b1626cc0ecef38d60a7fc36425
Randall Floyd <rdfloyd@indiana.edu>
2018-08-01 16:13:18 -0400

    Setting up factories for tests

---
+++ spec/factories/file_sets.rb
@@ -0,0 +1,21 @@
+FactoryBot.define do
+  # The ::FileSet model is defined in spec/internal/app/models by the
+  # curation_concerns:install generator.
+  factory :file_set, class: FileSet do
+
+    transient do
+      user { FactoryBot.create(:user) }
+      content nil
+    end
+
+    after(:create) do |file, evaluator|
+      if evaluator.content
+        Hydra::Works::UploadFileToFileSet.call(file, evaluator.content)
+      end
+    end
+
+    after(:build) do |file, evaluator|
+      file.apply_depositor_metadata(evaluator.user.user_key)
+    end
+  end
+end
