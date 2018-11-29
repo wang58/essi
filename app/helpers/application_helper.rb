658a24f2b808760e52e9cfd59d2b49b5e5bc0942
Andy Smith <andjsmit@iupui.edu>
2018-11-29 08:58:13 -0500

    Remove unneeded comments.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,5 @@
 module ApplicationHelper
   def display_for(role_name)
     yield if current_user.roles.include? Role.where(name: role_name).first
-    #yield if switch_user_select.present?
   end
 end
