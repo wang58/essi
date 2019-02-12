a4fa9ee8fa1b7b3822f30a40a39d16a57d7940f3
Adam Ploshay <aploshay@iu.edu>
2019-02-12 07:55:36 -0500

    [ESSI-23] Add runner, processor, and output file service for hOCR

--- config/initializers/add_hocr_file_set_derivatives.rb
+++ config/initializers/add_hocr_file_set_derivatives.rb
@@ -1,21 +1,27 @@
-Hyrax::FileSetDerivativesService.class_eval do
-
+module FileSetDerivativesServiceExtensions
   def create_derivatives(filename)
-    Rails.logger.info 'Create File Set Derivatives Hit'
+    super
     case mime_type
-      when *file_set.class.pdf_mime_types             then create_pdf_derivatives(filename)
-      when *file_set.class.office_document_mime_types then create_office_document_derivatives(filename)
-      when *file_set.class.audio_mime_types           then create_audio_derivatives(filename)
-      when *file_set.class.video_mime_types           then create_video_derivatives(filename)
       when *file_set.class.image_mime_types
-        create_image_derivatives(filename)
         create_hocr_derivatives(filename)
     end
   end
 
   private
+    def create_hocr_derivatives(filename)
+      return unless ESSI.config.dig(:essi, :create_hocr_files)
+      # FIXME: add language: parameter logic somewhere -- fileset model?
+      OCRRunner.create(filename,
+                       { source: :original_file,
+                         outputs: [{ label: "#{file_set.id}-hocr.hocr",
+                                     mime_type: 'text/html; charset=utf-8',
+                                     format: 'hocr',
+                                     container: 'extracted_text',
+                                     language: 'eng',
+                                     url: uri }]})
+    end
+end
 
-  def create_hocr_derivatives(filename)
-    Rails.logger.info "Time to create a hOCR file for #{filename}"
-  end
+Hyrax::FileSetDerivativesService.class_eval do
+  prepend FileSetDerivativesServiceExtensions
 end
