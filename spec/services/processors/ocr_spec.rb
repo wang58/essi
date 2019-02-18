246b7b76d04a8101582413faef2d06a6041d0747
Adam Ploshay <aploshay@iu.edu>
2019-02-18 15:37:53 -0500

    [ESSI-23] Add spec coverage

---
+++ spec/services/processors/ocr_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe Processors::OCR do
+  subject { described_class.new(source_path, directives) }
+
+  let(:source_path) { '' }
+
+  describe "#encode" do
+    it "executes the external utility" do
+      expect(described_class).to receive(:execute) { 0 }
+      described_class.encode('path', {}, 'output_file')
+    end
+  end
+
+  describe "#options_for" do
+    context "without a language directive" do
+      let(:directives) { {} }
+      it "returns a hash with a default tesseract language argument" do
+        expect(subject.options_for('hocr')&.dig(:options).to_s).to match /-l eng/
+      end
+    end
+    context "with a language directive" do
+      let(:directives) { { language: 'ita' } }
+      it "returns a hash with the specified tesseract language argument" do
+        expect(subject.options_for('hocr')&.dig(:options).to_s).to match /-l ita/
+      end
+    end
+  end
+end
