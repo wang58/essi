fb0170c652c55b74651bb4183d65a4cf64aaaf5c
Richard Wing <rwing@iupui.edu>
2019-03-15 18:18:05 -0400

    Added checkmark and crossmark to File Manager full text field.

--- app/helpers/file_manager_helper.rb
+++ app/helpers/file_manager_helper.rb
@@ -1,6 +1,9 @@
 module FileManagerHelper
   def ocr_check(id)
-    return 'YES' if ::FileSet.find(id).extracted_text
-    'NO'
+    checkmark = "\u2705" # Green boxed checkmark
+    crossmark = "\u274C" # Red crossmark
+
+    return checkmark.encode('utf-8') if ::FileSet.find(id).extracted_text
+    crossmark.encode('utf-8')
   end
 end
