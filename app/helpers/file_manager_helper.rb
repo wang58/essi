8ba0cd3b8b7c713459e29e2796546f4bef4b48ff
Richard Wing <rwing@iupui.edu>
2019-03-21 14:47:58 -0400

    Simplified filee_manager_helper.rb

--- app/helpers/file_manager_helper.rb
+++ app/helpers/file_manager_helper.rb
@@ -1,9 +1,6 @@
 module FileManagerHelper
   def ocr_check(id)
-    checkmark = "\u2705" # Green boxed checkmark
-    crossmark = "\u274C" # Red crossmark
-
-    return checkmark.encode('utf-8') if ::FileSet.find(id).extracted_text
-    crossmark.encode('utf-8')
+    return '✅' if ::FileSet.find(id).extracted_text
+    '❌'
   end
 end
