fa660306a370e3bbf4db87fe8989087de89fb3a1
Andy Smith <andjsmit@iupui.edu>
2019-03-22 09:51:54 -0400

    Adds test coverage for file manager helper.

--- spec/helpers/file_manager_helper.spec.rb
+++ spec/helpers/file_manager_helper.spec.rb
@@ -1,17 +1,20 @@
 require 'rails_helper'
 
 describe FileManagerHelper do
-  let(:file_set) { FactoryBot.create(:file_set) }
+
   describe '#ocr_check(:id)' do
-    context 'when fileset has extracted text' do
-      it 'returns a checkmark' do
-        expect(helper.ocr_check(file_set.id)).to eq('NO')
+    context 'when fileset does not have extracted text' do
+      it 'returns an X mark' do
+        mock_fileset = double('FileSet', extracted_text: nil)
+        allow(FileSet).to receive(:find).with('123abc').and_return(mock_fileset)
+        expect(helper.ocr_check('123abc')).to eq('NO')
       end
     end
-    context 'when fileset does not have extracted text' do
-      it 'returns an x mark' do
-        allow(file_set).to receive(:extracted_text) { 'Something' }
-        expect(helper.ocr_check(file_set.id)).to eq('YES')
+    context 'when fileset does have extracted text' do
+      it 'returns a check mark' do
+        mock_fileset = double('FileSet', extracted_text: 'Something')
+        allow(FileSet).to receive(:find).with('987zyx').and_return(mock_fileset)
+        expect(helper.ocr_check('987zyx')).to eq('YES')
       end
     end
   end
