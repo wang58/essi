0056ec22ee60be383dddcd5152eed38837bf9259
Daniel Pierce <dlpierce@indiana.edu>
2019-05-10 13:34:32 -0400

    Revert "Use separate redis server for sidekiq, and do not use redis namespaces"
    This reverts commit 77c0682a

--- config/initializers/sidekiq.rb
+++
@@ -1,7 +0,0 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: ESSI.config[:sidekiq][:url] }
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = { url: ESSI.config[:sidekiq][:url] }
-end
