658b91f690bb279b85191dd49d12b9781b7d5ba7
Andy Smith <andjsmit@iupui.edu>
2019-05-14 10:04:17 -0400

    Adds viewing hint to iiif manifest canvases.

--- config/initializers/add_viewing_hint_to_file_sets.rb
+++ config/initializers/add_viewing_hint_to_file_sets.rb
@@ -17,3 +17,22 @@ end
 Hyrax::Forms::FileSetEditForm.class_eval do
   include FileSetEditFormAddViewingHints
 end
+
+module ImageBuilderAddViewingHints
+  def apply(canvas)
+    annotation['on'] = canvas['@id']
+    canvas['width'] = annotation.resource['width']
+    canvas['height'] = annotation.resource['height']
+    canvas['viewingHint'] = canvas_viewing_hint(canvas)
+    canvas.images += [annotation]
+  end
+
+  def canvas_viewing_hint(canvas)
+    id = canvas['@id'].split('/')[-1]
+    FileSet.find(id).viewing_hint
+  end
+end
+
+IIIFManifest::ManifestBuilder::ImageBuilder.class_eval do
+  prepend ImageBuilderAddViewingHints
+end
