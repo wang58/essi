309c7cd797489873f5def82155f5a618a91c9e7e
Adam Ploshay <aploshay@iu.edu>
2019-07-23 15:50:58 -0400

    [ESSI-260] Add NewspaperWorks libraries to support ALTO word boundaries

---
+++ lib/newspaper_works.rb
@@ -0,0 +1,4 @@
+require 'newspaper_works/text_extraction'
+
+module NewspaperWorks
+end
---
+++ lib/newspaper_works/text_extraction.rb
@@ -0,0 +1,8 @@
+require 'newspaper_works/text_extraction/alto_reader'
+require 'newspaper_works/text_extraction/word_coords_builder'
+
+module NewspaperWorks
+  # Module for text extraction (OCR or otherwise)
+  module TextExtraction
+  end
+end
