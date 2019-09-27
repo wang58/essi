3c5397babbef344a2aee8551267e9386b6f45f59
Ying Wang <wang58@iu.edu>
2019-09-27 12:00:53 -0400

    ESSI-413 uv download issue - copied displays_image.rb from hyrax

--- app/presenters/hyrax/displays_image.rb
+++ app/presenters/hyrax/displays_image.rb
@@ -21,8 +21,8 @@ module Hyrax
       )
       # @see https://github.com/samvera-labs/iiif_manifest
       IIIFManifest::DisplayImage.new(url,
-                                     width: original_file.width,
-                                     height: original_file.height,
+                                     width: 640,
+                                     height: 480,
                                      iiif_endpoint: iiif_endpoint(original_file.id))
     end
 
