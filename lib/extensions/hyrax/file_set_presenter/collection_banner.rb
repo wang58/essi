40ef3265c0eb8d476bcfd76a6c994e87a7f03aee
Andy Smith <andjsmit@iupui.edu>
2020-02-04 08:58:49 -0500

    Fix error when file set is not in a collection. Add styles for collections with no banner.

--- lib/extensions/hyrax/file_set_presenter/collection_banner.rb
+++ lib/extensions/hyrax/file_set_presenter/collection_banner.rb
@@ -3,7 +3,7 @@ module Extensions
     module FileSetPresenter
       module CollectionBanner
         def collection
-          # return fileset collection if any
+          # return fileset collection if any, else nil
           FileSet.find(id).parent.member_of_collections.first
         end
       end
