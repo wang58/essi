ae0cdd76bdcb14d579e8b3deb544ca1e9005a26f
Adam Ploshay <aploshay@iu.edu>
2020-04-16 09:44:23 -0400

    [ESSI-759] [ESSI-809] Modularize remote metadata form changes

--- app/forms/hyrax/paged_resource_form.rb
+++ app/forms/hyrax/paged_resource_form.rb
@@ -4,9 +4,10 @@ module Hyrax
   # Generated form for PagedResource
   class PagedResourceForm < Hyrax::Forms::WorkForm
     self.model_class = ::PagedResource
-    self.terms += [:resource_type, :source_metadata_identifier, :series]
+    self.terms += [:resource_type, :series]
     self.required_fields -= [:title, :creator, :keyword]
-    self.primary_fields = [:title, :creator, :rights_statement, :source_metadata_identifier]
+    self.primary_fields = [:title, :creator, :rights_statement]
     include ESSI::PagedResourceFormBehavior
+    include ESSI::RemoteMetadataFormElements
   end
 end
