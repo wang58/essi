943f375b42e5aca7291fb6b7939019c6c60d3a3a
Daniel Pierce <dlpierce@indiana.edu>
2020-04-30 11:17:42 -0400

    Add thumbnail_id to FileSetPresenter to match other presenters.

--- lib/extensions/hyrax/file_set_presenter/viewing_hint.rb
+++ lib/extensions/hyrax/file_set_presenter/viewing_hint.rb
@@ -2,7 +2,7 @@ module Extensions
   module Hyrax
     module FileSetPresenter
       module ViewingHint
-        delegate :viewing_hint, to: :solr_document
+        delegate :viewing_hint, :thumbnail_id, to: :solr_document
         #@todo remove after upgrade to Hyrax 3.x
         delegate :original_file_id, to: :solr_document
       end
