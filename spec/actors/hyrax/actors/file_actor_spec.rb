329bdd3f4c3985dba104ab36e3e9ca925c26bb9c
Adam Ploshay <aploshay@iu.edu>
2020-05-01 11:26:58 -0400

    [ESSI-704] [ESSI-819] Refactor spec for FileActor

--- spec/actors/hyrax/actors/file_actor_spec.rb
+++ spec/actors/hyrax/actors/file_actor_spec.rb
@@ -9,46 +9,49 @@ describe Hyrax::Actors::FileActor do
   let(:file) { File.new(filename) }
   let(:io) { JobIoWrapper.create_with_varied_file_handling!(user: user, file: file, relation: :original_file, file_set: file_set) }
 
-  context 'when :store_original_files is false', :clean do
-    it 'sets the mime_type to an external body redirect' do
-      expect(CharacterizeJob).not_to receive(:perform_later)
-      allow(ESSI.config).to receive(:dig) \
-        .with(any_args).and_call_original
-      allow(ESSI.config).to receive(:dig) \
-        .with(:essi, :store_original_files) \
-        .and_return(false)
-      allow(ESSI.config).to receive(:dig) \
-        .with(:essi, :create_hocr_files) \
+  describe '#ingest_file' do
+    before do
+      allow(ESSI.config).to receive(:dig).with(any_args).and_call_original
+      allow(ESSI.config).to receive(:dig).with(:essi, :create_hocr_files) \
         .and_return(true)
-      allow(ESSI.config).to receive(:dig) \
-        .with(:essi, :index_hocr_files) \
+      allow(ESSI.config).to receive(:dig).with(:essi, :index_hocr_files) \
         .and_return(true)
-      allow(ESSI.config).to receive(:dig) \
-        .with(:essi, :master_file_service_url) \
-        .and_return('http://service')
-      file_actor.ingest_file(io)
-      expect(file_set.reload.original_file.mime_type).to include \
-        ESSI.config.dig :essi, :master_file_service_url
     end
-  end
-
-  context 'when :store_original_files is true', :clean do
-    it 'saves an image file to the member file_set' do
-      expect(CharacterizeJob).to receive(:perform_later) \
-        .with(file_set, String, String)
-      allow(ESSI.config).to receive(:dig) \
-        .with(any_args).and_call_original
-      allow(ESSI.config).to receive(:dig) \
-        .with(:essi, :store_original_files) \
-        .and_return(true)
-      allow(ESSI.config).to receive(:dig) \
-        .with(:essi, :create_hocr_files) \
-        .and_return(true)
-      allow(ESSI.config).to receive(:dig) \
-        .with(:essi, :index_hocr_files) \
-        .and_return(true)
-      file_actor.ingest_file(io)
-      expect(file_set.reload.original_file.mime_type).to include "image/png"
+    context 'when :store_original_files is false', :clean do
+      before do
+        allow(ESSI.config).to receive(:dig)
+          .with(:essi, :store_original_files) \
+          .and_return(false)
+        allow(ESSI.config).to receive(:dig) \
+          .with(:essi, :master_file_service_url) \
+          .and_return('http://service')
+      end
+      it 'sets the mime_type to an external body redirect' do
+        file_actor.ingest_file(io)
+        expect(file_set.reload.original_file.mime_type).to include \
+          ESSI.config.dig :essi, :master_file_service_url
+      end
+      it 'does not run characterization' do
+        expect(CharacterizeJob).not_to receive(:perform_later)
+        file_actor.ingest_file(io)
+      end
+    end
+  
+    context 'when :store_original_files is true', :clean do
+      before do
+        allow(ESSI.config).to receive(:dig) \
+          .with(:essi, :store_original_files) \
+          .and_return(true)
+      end
+      it 'saves an image file to the member file_set' do
+        file_actor.ingest_file(io)
+        expect(file_set.reload.original_file.mime_type).to include "image/png"
+      end
+      it 'runs characterization' do
+        expect(CharacterizeJob).to receive(:perform_later) \
+          .with(file_set, String, String)
+        file_actor.ingest_file(io)
+      end
     end
   end
 end
